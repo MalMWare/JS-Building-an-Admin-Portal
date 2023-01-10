558f5b2f81a84ec52d78a86594d5d1e844c0238c
MalMWare <malkelleymartin@gmail.com>
2023-01-09 23:40:52 -0800

    finished

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,32 @@
+let root = document.querySelector('#root')
 
-// Your Code Here
+const mainAdmin = async () => {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    let ul = document.createElement('ul')
+
+    books.forEach(function(m) {
+        let li = document.createElement('li')
+        
+        let input = document.createElement('input')
+        input.type = 'text'
+        input.value = m.quantity 
+
+        let button = document.createElement('button')
+        button.textContent = 'Save'
+        button.addEventListener('click', function(){
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'id': m.id, 'quantity': `${input.value}`
+            })})
+        })
+
+        li.append(m.title, input, button)
+        ul.append(li)
+    })
+    root.append(ul)
+}
+
+mainAdmin()
